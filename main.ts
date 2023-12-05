f8d3d7c448549dec1f51b135778b40ca7cf539b2
Henryc <72913927+Henryy417@users.noreply.github.com>
2023-12-05 23:34:18 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -3,7 +3,7 @@ radio.setTransmitPower(7)
 radio.setFrequencyBand(0)
 
 radio.onReceivedString(function (msg) {
+    music.ringTone(Note.C);
     let msgArray = msg.split(":");
     basic.showString("Safevest " + msgArray[0] + " has detected " + msgArray[1] + ".");
-    music.ringTone(Note.C);
 })
