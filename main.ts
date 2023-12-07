1a30ee647b5fa2610a6ccb66a16f21022e2f95e0
Henryc <72913927+Henryy417@users.noreply.github.com>
2023-12-07 23:42:29 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -5,5 +5,34 @@ radio.setFrequencyBand(0)
 radio.onReceivedString(function (msg) {
     music.ringTone(Note.C);
     let msgArray = msg.split(":");
-    basic.showString("Safevest " + msgArray[0] + " has detected " + msgArray[1] + ".");
+    basic.showString("SafeVest " + msgArray[0] + " has detected " + msgArray[1] + ".");
 })
+
+// Button click counter
+let buttonClickCount = 0;
+let lastButtonClick = 0;
+input.onButtonPressed(Button.A, () => {
+    if (input.runningTime() - lastButtonClick > 1000) {
+        lastButtonClick = 0;
+    }
+    buttonClickCount++;
+    lastButtonClick = input.runningTime();
+});
+function getButtonClickCount(): number | void {
+    if (input.runningTime() - lastButtonClick > 1000) {
+        let temp = buttonClickCount;
+        buttonClickCount = 0;
+        return temp;
+    }
+}
+
+basic.forever(() => {
+    let notifyID = getButtonClickCount();
+    if (typeof notifyID == "number" && notifyID > 0)
+    {
+        radio.sendNumber(notifyID);
+        basic.showString("SafeVest " + notifyID + " notified.");
+        pause(5000);
+        basic.clearScreen();
+    }
+});
